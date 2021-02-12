32002ea1f6e29b0080df9b8514c9c9564a4bd9ad
Viviana Monsalve <viviana.monsalve.a@gmail.com>
2021-02-11 22:59:03 -0500

    build script added

--- sources/build.sh
+++ sources/build.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p ../fonts/otf ../fonts/ttf
 
 echo "Generating static TTF"
-fontmake -g TokyoZoo.glyphs -i -o ttf --output-dir ../fonts/ttf/ -a
+fontmake -g TokyoZoo.glyphs -i -o ttf -f --output-dir ../fonts/ttf/ -a
 
 echo "Post processing static TTF"
 ttfs=$(ls ../fonts/ttf/*.ttf)
@@ -15,14 +15,14 @@ do
 	mv "$ttf.fix" $ttf
 done
 
-# echo "Generating static OTFs"
-# fontmake -g Dots.glyphs -i -o otf -f --output-dir ../fonts/otf/ -a
+echo "Generating static OTFs"
+fontmake -g TokyoZoo.glyphs -i -o otf -f --output-dir ../fonts/otf/ -a
 
-# echo "Post processing static OTFs"
-# otf=$(ls ../fonts/otf/*.otf)
-# for otf in $otf
-# do
-# 	gftools fix-dsig -f $otf
-# done
+echo "Post processing static OTFs"
+otf=$(ls ../fonts/otf/*.otf)
+for otf in $otf
+do
+	gftools fix-dsig -f $otf
+done
 
 rm -rf master_ufo/ instance_ufo/ ../fonts/ttf/*backup*.ttf 
